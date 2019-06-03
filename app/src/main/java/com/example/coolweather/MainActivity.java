a964b3331305077701a08d7dee291400638b4b92
18810372263 <https://github.com/18810372263/coolweather.git>
2019-06-03 15:31:16 +0800

    Test

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MainActivity extends AppCompatActivity {
+    static final String Tag = "MainActivity";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
