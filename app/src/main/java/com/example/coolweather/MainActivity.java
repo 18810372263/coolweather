f03f14e21c5e5cbafa911e2682675c5c773e25fd
lj <1172886876@qq.com>
2019-06-03 15:00:22 +0800

    First commit

---
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.coolweather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JiangLi on 2019/6/3.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
